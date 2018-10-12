ecb523a2197aead5d767ff96c02c13acb9665abf
Alina Constantinescu <alinaconstantinescu@Alinas-MacBook-Pro.local>
2018-10-12 18:34:31 +0300

    commit site

---
+++ Authors site/main.js
@@ -0,0 +1,7 @@
+var submenu = document.getElementsByClassName('submenu');
+
+for (i = 0; i < submenu.length; i++) {
+    submenu[i].addEventListener('click', showBooks() {
+        this.class
+    })
+}
